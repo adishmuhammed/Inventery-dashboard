b1a68e2680ca73690cdde04d1994ace6951f50e2
adishmuhammed <adishmuhammedmm@gmail.com>
2023-03-25 10:32:19 +0530

    error page update

---
+++ resources/views/error/503.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+        content="Erratum – Multi purpose error page template for Service, corporate, agency, Consulting, startup.">
+    <meta name="keywords" content="Error page 404, page not found design, wrong url">
+    <meta name="author" content="Ashishmaraviya">
+    <link rel="icon" href="{{ asset('/assets/images/favicon.png') }}" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{ asset('/assets/images/favicon.png') }}" type="image/x-icon" />
+    <title>500 - Pensare is developing site</title>
+    <!--Google font-->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300&display=swap"
+        rel="stylesheet">
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/bootstrap.css') }}">
+    <!-- Theme css -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/error-page.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/error-page-responsive.css') }}">
+</head>
+
+<body>
+    <!-- 01 Preloader -->
+    <div class="loader-wrapper" id="loader-wrapper">
+        <div class="loader"></div>
+    </div>
+    <!-- Preloader end -->
+    <!-- 02 Main page -->
+    <section class="page-section">
+        <div class="full-width-screen">
+            <div class="container-fluid p-0">
+                <div class="content-detail p-0">
+                    <div class="container-txt p-0">
+                        <h1><span>500</span></h1>
+                        <div class="blobs_1"></div>
+                        <div class="blobs_2"></div>
+                        <div class="blobs_3"></div>
+                        <div class="blobs_4"></div>
+                        <div class="blobs_5"></div>
+                        <div class="blobs_6"></div>
+                        <div class="blobs_7"></div>
+                    </div>
+                    <h4 class="sub-title">the website is in development preview</h4>
+
+                    <p class="detail-text">Internal Server Error,<br> The server cannot process the request for an
+                        unknown reason.</p>
+
+                    <div class="back-btn">
+                        <a href="{{ url('index.html') }}" class="btn">Back to Home</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- latest jquery-->
+    <script src="{{ asset('/assets/js/jquery-3.5.1.min.js') }}"></script>
+    <!-- Theme js-->
+    <script src="{{ asset('/assets/js/script.js') }}"></script>
+</body>
+
+</html>
