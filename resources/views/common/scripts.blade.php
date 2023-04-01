060c73269135730bbad2790335b431cfd11ece4e
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-01 12:00:34 +0530

    maked the text littile more contrast and removed maps api as no longer needed

--- resources/views/common/scripts.blade.php
+++ resources/views/common/scripts.blade.php
@@ -6,8 +6,5 @@
  <!-- Plugins for this template -->
  <script src="{{ asset('assets/js/jquery-plugin-collection.js') }}"></script>
 
- <!-- Google map api -->
- <script src="https://maps.googleapis.com/maps/api/js?key"></script>
-
  <!-- Custom script for this template -->
  <script src="{{ asset('assets/js/script.js') }}"></script>
