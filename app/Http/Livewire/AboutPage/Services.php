cce98ecae0ba93bd11e41fc722a71deb054b2b41
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-26 18:08:12 +0530

    services section compleate

---
+++ app/Http/Livewire/AboutPage/Services.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\AboutPage;
+
+use Livewire\Component;
+
+class Services extends Component
+{
+    public function render()
+    {
+        return view('livewire.about-page.services');
+    }
+}
---
+++ resources/views/livewire/about-page/services.blade.php
@@ -0,0 +1,54 @@
+<div class="about-service paddingub text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="about-serv-detail">
+                    <img src="{{ asset('images/logo/Chronic-Kidney-Disease') }}" alt="health-icon" class="mb-3 animall">
+                    <h4 class="ser-title">Chronic Kidney Disease</h4>
+                    <p class="ser-p">We provide comprehensive care to people with kidney disorders with a dedicated team
+                        of providers</p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="about-serv-detail">
+                    <img src="{{ asset('images/logo/Dialysis-Training') }}" alt="adoption-icon" class="mb-3 animall">
+                    <h4 class="ser-title">Dialysis Training</h4>
+                    <p class="ser-p">We offer training and support for both home hemodialysis and peritoneal dialysis
+                    </p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="about-serv-detail">
+                    <img src="{{ asset('images/logo/Hemodialysis') }}" alt="education-icon" class="mb-3 animall">
+                    <h4 class="ser-title">Hemodialysis</h4>
+                    <p class="ser-p">We perform hemodialysis in our state-of-the-art dialysis center with modern
+                        facilities and equipment</p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="about-serv-detail">
+                    <img src="{{ asset('images/logo/Outpatient-Dialysis') }}" alt="inspiration-icon"
+                        class="mb-3 animall">
+                    <h4 class="ser-title">Outpatient Dialysis</h4>
+                    <p class="ser-p">We provide dialysis services to patients who do not require hospitalization</p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="about-serv-detail">
+                    <img src="{{ asset('images/logo/Hemofiltration') }}" alt="volunt-icon" class="mb-3 animall">
+                    <h4 class="ser-title">Hemofiltration</h4>
+                    <p class="ser-p">We offer alternative treatments to hemodialysis for adult and pediatric patients
+                    </p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="about-serv-detail">
+                    <img src="{{ asset('images/logo/Peritoneal-Dialysis') }}" alt="donat-icon" class="mb-3 animall">
+                    <h4 class="ser-title">Peritoneal Dialysis</h4>
+                    <p class="ser-p">We provide peritoneal dialysis, a type of dialysis that uses the lining of the
+                        abdomen as a filter</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
