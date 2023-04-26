0194471b419d3033bd85da9679907a31a400fbac
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-26 16:02:58 +0530

    asseting sponcers

--- resources/views/livewire/homepage/sponsers.blade.php
+++ resources/views/livewire/homepage/sponsers.blade.php
@@ -3,28 +3,28 @@
         <div class="row">
             <div class="col-lg-12 brand slider-width" id="owl-brand-slider">
                 <div class="item">
-                    <img src="image/brand1.png" alt="brand">
+                    <img src="{{ asset('image/brand1.png') }}" alt="brand">
                 </div>
                 <div class="item">
-                    <img src="image/brand2.png" alt="brand">
+                    <img src="{{ asset('image/brand2.png') }}" alt="brand">
                 </div>
                 <div class="item">
-                    <img src="image/brand3.png" alt="brand">
+                    <img src="{{ asset('image/brand3.png') }}" alt="brand">
                 </div>
                 <div class="item">
-                    <img src="image/brand4.png" alt="brand">
+                    <img src="{{ asset('image/brand4.png') }}" alt="brand">
                 </div>
                 <div class="item">
-                    <img src="image/brand5.png" alt="brand">
+                    <img src="{{ asset('image/brand5.png') }}" alt="brand">
                 </div>
                 <div class="item">
-                    <img src="image/brand6.png" alt="brand">
+                    <img src="{{ asset('image/brand6.png') }}" alt="brand">
                 </div>
                 <div class="item">
-                    <img src="image/brand1.png" alt="brand">
+                    <img src="{{ asset('image/brand1.png') }}" alt="brand">
                 </div>
                 <div class="item">
-                    <img src="image/brand2.png" alt="brand">
+                    <img src="{{ asset('image/brand2.png') }}" alt="brand">
                 </div>
             </div>
         </div>
