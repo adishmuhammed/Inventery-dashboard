fa0adadd7c5e21167112abbe31073bb87240ef66
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-27 11:11:26 +0530

    what i could do

---
+++ app/Http/Livewire/LabTechnician/WhyCta.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\LabTechnician;
+
+use Livewire\Component;
+
+class WhyCta extends Component
+{
+    public function render()
+    {
+        return view('livewire.lab-technician.why-cta');
+    }
+}
