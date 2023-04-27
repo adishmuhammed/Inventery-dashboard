553c8b05aafb8c7c9940b6e69cec390538d182c7
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-27 11:08:41 +0530

    h

---
+++ app/Http/Livewire/LabTechnician.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LabTechnician extends Component
+{
+    public function render()
+    {
+        return view('livewire.lab-technician');
+    }
+}
