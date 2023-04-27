2156d9467d5d3528af5e1a91085b46da52849ada
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-27 11:03:22 +0530

    why ctc done and the page is done

---
+++ app/Http/Livewire/DiplomaInDialysisTechnology/WhyCta.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\DiplomaInDialysisTechnology;
+
+use Livewire\Component;
+
+class WhyCta extends Component
+{
+    public function render()
+    {
+        return view('livewire.diploma-in-dialysis-technology.why-cta');
+    }
+}
