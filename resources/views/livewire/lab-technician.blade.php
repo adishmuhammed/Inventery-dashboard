fc744d8b7eb841ad89198e262180ff0e19a5774f
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-27 11:10:06 +0530

    bulck of the change

--- resources/views/livewire/lab-technician.blade.php
+++ resources/views/livewire/lab-technician.blade.php
@@ -1,3 +1,25 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    <livewire:components.page-start title="Cources" />
+    <main class="cause-detail pr-100 paddingub">
+        <div class="container">
+            <livewire:diploma-in-dialysis-technology.heading title="Lab Technician"
+                university="University" fee="1200" />
+            <div class="text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat qui ducimus illum modi? Libero saepe
+                perspiciatis accusamus soluta perferendis, ad illum, nesciunt, reiciendis iusto et cupiditate.
+                Repudiandae provident, consectetur, sapiente, libero iure necessitatibus corporis nulla voluptate,
+                quisquam aut eum perspiciatis? Fugiat labore aspernatur eius, perspiciatis ut molestiae, delectus
+                rem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat qui ducimus illum modi? Libero
+                saepe perspiciatis accusamus soluta perferendis, ad illum, nesciunt, reiciendis iusto et cupiditate.
+                Repudiandae provident, consectetur, sapiente, libero iure necessitatibus corporis nulla voluptate,
+                quisquam aut eum perspiciatis? Fugiat labore aspernatur eius, perspiciatis ut molestiae, delectus
+                rem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat qui ducimus illum modi? Libero
+                saepe perspiciatis accusamus soluta perferendis, ad illum, nesciunt, reiciendis iusto et cupiditate.
+                Repudiandae provident, consectetur, sapiente, libero iure necessitatibus corporis nulla voluptate,
+                quisquam aut eum perspiciatis? Fugiat labore aspernatur eius, perspiciatis ut molestiae, delectus rem.
+            </div>
+            <livewire:diploma-in-dialysis-technology.why-cta />
+            <livewire:components.footer />
+        </div>
+    </main>
 </div>
