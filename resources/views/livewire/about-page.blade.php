054c2147b3f1cec8480245a29c2356ced15d0b69
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-27 10:21:05 +0530

    donate cta

--- resources/views/livewire/about-page.blade.php
+++ resources/views/livewire/about-page.blade.php
@@ -19,4 +19,5 @@
         'Studies show that the number of kidney patients is increasing in Kerala. The reasons can be many. But there are two solutions ahead. Either undergo a kidney transplant. It is not that easy. There will be a huge financial burden. Ordinary people cannot afford it. The second option is dialysis until death. Poonur Ayesha Hussain Dialysis Center is an institution that provides free and free dialysis to kidney patients according to their financial status. More than 350 kidney patients undergo dialysis every month in this institution, which is run under the SYS Kozhikode District Committee. Hoping for your support and prayers for this great initiative for poor people.',
     ]" />
     <livewire:about-page.services />
+    <livewire:about-page.donate-cta />
 </div>
