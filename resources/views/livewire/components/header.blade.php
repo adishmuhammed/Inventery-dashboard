7cc0f241f3a944dc29ff2f59655c87d6471f1be8
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-28 09:44:49 +0530

    fixing the navigation bar compleatly

--- resources/views/livewire/components/header.blade.php
+++ resources/views/livewire/components/header.blade.php
@@ -1,14 +1,20 @@
-<header class="relative z-20 w-full bg-gradient-to-r">
-    <nav class="flex md:justify-center justify-around gap-6 items-center p-3" x-data="{ mobileMenuOpen: @entangle('mobileMenuOpen') }">
+<header class="relative z-20 w-full bg-gradient-to-r" x-data="{ mobileMenuOpen: @entangle('mobileMenuOpen') }">
+    <nav class="flex md:justify-center justify-around gap-6 items-center p-3" x-data="{ open: @entangle('isOpen') }">
         <div class="flex items-center gap-3">
             <img class="w-12 bg-white rounded-full" src="{{ asset('images/logo.webp') }}" alt="">
             <h3 class="">Sahayi Dialysis Center</h3>
         </div>
         <a class="hidden md:block no-underline text-center" href="{{ route('home') }}">Home</a>
         <a class="hidden md:block no-underline text-center" href="{{ route('about') }}">About</a>
-        <div class="hidden md:block">
-            <livewire:small-components.dropdown-menu :items="['Diploma in Dialysis Technology', 'Lab Technician']" heading="Courses" :links[route('ddt'),
-                route('lab-tech')] />
+        <button @click="open = !open" class=" text-black py-2 rounded hidden md:block">
+            Courses
+        </button>
+        <div x-show="open" @click.away="open = false" class="absolute top-20 mt-2 w-48 bg-white rounded shadow-md">
+            <ul class="text-gray-700">
+                <li class="px-4 py-2 hover:bg-gray-200"><a href="{{ route('ddt') }}">Diploma in Dialysis
+                        Technology</a></li>
+                <li class="px-4 py-2 hover:bg-gray-200"><a href="{{ route('lab-tech') }}">Lab Technician</a></li>
+            </ul>
         </div>
         <a class="hidden md:block no-underline text-center" href="{{ route('gallery') }}">Gallery</a>
         <a class="hidden md:block no-underline text-center" href="">Blog</a>
@@ -27,7 +33,7 @@
             @click="mobileInnerSubMenuOpen = !mobileInnerSubMenuOpen">
             Cources
         </button>
-        <div x-show="mobileInnerSubMenuOpen" @click.away="mobileInnerSubMenuOpen == false" class="text-gray-400">
+        <div x-show="mobileInnerSubMenuOpen" @click.away="mobileInnerSubMenuOpen = false" class="text-gray-400">
             <a class="py-3 border-t border-b no-underline block" href="{{ route('ddt') }}">Diploma in Dialysis
                 Technology</a>
             <a class="py-3 border-t border-b no-underline block" href="{{ route('lab-tech') }}">Lab Technician</a>
