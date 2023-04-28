44b4ed77711ca22fa92b7f7bb8aac2c3ccdb3655
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-28 11:31:05 +0530

    blogs  complete

--- app/Http/Livewire/BlogsPage.php
+++ app/Http/Livewire/BlogsPage.php
@@ -3,11 +3,21 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class BlogsPage extends Component
 {
+    use WithPagination;
+
+    public $blogs;
+
+    public function mount()
+    {
+        $this->blogs = Blog::latest()->paginate(12);
+    }
+    
     public function render()
     {
-        return view('livewire.blogs-page');
+        return view('livewire.blogs-page')->with('blogs', $this->blogs);
     }
 }
--- resources/views/livewire/blogs-page.blade.php
+++ resources/views/livewire/blogs-page.blade.php
@@ -2,7 +2,9 @@
     <livewire:components.header />
     <livewire:components.page-start title="Blogs" />
     <main class="container">
-        <livewire:components.card />
-    </main> 
+        @foreach ($blogs as $blog)
+            <livewire:components.card :blog="$blog" />
+        @endforeach
+    </main>
     <livewire:components.footer />
 </div>
