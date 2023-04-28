6477c8ee728370547532cd84531c6bf2730f9cd0
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-28 09:48:08 +0530

    this page also done

--- resources/views/livewire/lab-technician.blade.php
+++ resources/views/livewire/lab-technician.blade.php
@@ -3,7 +3,7 @@
     <livewire:components.page-start title="Cources" />
     <main class="cause-detail pr-100 paddingub">
         <div class="container">
-            <livewire:diploma-in-dialysis-technology.heading title="Lab Technician" university="University"
+            <livewire:diploma-in-dialysis-technology.heading heading="Lab Technician" university="University"
                 fee="1200" />
             <div class="text">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat qui ducimus illum modi? Libero saepe
@@ -20,7 +20,7 @@
                 quisquam aut eum perspiciatis? Fugiat labore aspernatur eius, perspiciatis ut molestiae, delectus rem.
             </div>
             <livewire:lab-technician.why-cta />
-            <livewire:components.footer />
         </div>
     </main>
+    <livewire:components.footer />
 </div>
