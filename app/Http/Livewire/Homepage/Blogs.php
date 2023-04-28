72b97857c2b1a89649c80e7bbde081cd3f075837
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-28 11:14:32 +0530

    the homepage blog backend compleate

--- app/Http/Livewire/Homepage/Blogs.php
+++ app/Http/Livewire/Homepage/Blogs.php
@@ -3,11 +3,23 @@
 namespace App\Http\Livewire\Homepage;
 
 use Livewire\Component;
+use App\Models\Blog;
+use Livewire\WithPagination;
 
 class Blogs extends Component
 {
+    use WithPagination;
+    public $blogs;
+
+    public function mount()
+    {
+        $this->blogs = Blog::orderBy('created_at', 'desc')
+            ->take(3)
+            ->get();
+    }
+
     public function render()
     {
-        return view('livewire.homepage.blogs');
+        return view('livewire.homepage.blogs', compact('blogs'));
     }
 }
