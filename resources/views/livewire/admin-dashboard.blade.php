05cc8ebdfd981ee04103eed95c8d5e9b471fc3af
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-29 11:38:36 +0530

    the login page completed

---
+++ resources/views/livewire/admin-dashboard.blade.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <div class="flex justify-center align-middle h-6">
+        <b>Admin </b>
+    </div>
+</div>
---
+++ resources/views/livewire/login-page.blade.php
@@ -0,0 +1,8 @@
+<div class="grid place-content-center">
+    <div class="border shadow-xl flex flex-col gap-3 align-middle p-10">
+        <h1 class="text-center">Login</h1>
+        <input type="text" placeholder="Username" name="username">
+        <input type="password" placeholder="Password" name="password">
+        <button class="bg-cyan-600 p-3 rounded" type="submit">Login</button>
+    </div>
+</div>
