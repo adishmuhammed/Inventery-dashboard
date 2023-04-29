bf970be33630a7ccc228c9959b2436dcb23828be
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-29 11:36:43 +0530

    blogs  complete

--- resources/views/livewire/components/card.blade.php
+++ resources/views/livewire/components/card.blade.php
@@ -4,7 +4,7 @@
         class="flex flex-col space-y-3 items-start justify-end w-full sm:w-auto sm:ml-6  sm:mt-0 pl-4 pr-5 py-5 border border-gray-200">
         <h3 class="w-full text-base leading-tight text-gray-800">{{ $blog->title }}
         </h3>
-        <p class="w-full text-sm leading-tight text-gray-400 hidden sm:block">{{ $blog->shortDescribtion }}</p>
-        <a href="{{ route('blog-detail') }}" class="w-full text-sm leading-tight text-green-500">Read more</a>
+        <p class="w-full text-sm leading-tight text-gray-400 hidden sm:block">{{ substr($blog->content, 0, 126) }}</p>
+        <a href="{{ route('blog-detail', $blog) }}" class="w-full text-sm leading-tight text-green-500">Read more</a>
     </div>
 </div>
