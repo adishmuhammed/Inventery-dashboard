ff37ace7279e8b391d49cd67837bc575fd35a6a2
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-29 11:38:37 +0530

    message section of the admin page

--- app/Http/Livewire/AdminDashboard.php
+++ app/Http/Livewire/AdminDashboard.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class AdminDashboard extends Component
 {
+    public  $message;
+    public $blog;
+    public $gallery;
+    public $contactMessages;
+    public $galleryImages;
+    public $galleryVideos;
+    public $blogPosts;
     public function render()
     {
         return view('livewire.admin-dashboard');
