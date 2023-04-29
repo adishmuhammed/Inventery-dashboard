1c12ae5fb968516c9545d8bf3d2ff9ab8cc7bf71
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-29 11:38:36 +0530

    loging the user

--- app/Http/Livewire/LoginPage.php
+++ app/Http/Livewire/LoginPage.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class LoginPage extends Component
 {
+    public $username;
+    public $password;
     public function render()
     {
         return view('livewire.login-page');
+    }
+
+    public function Login()
+    {
+        if (Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+            return redirect()->intended('/dashboard');
+        } else {
+            return back()->withErrors(['email' => 'The provided credentials do not match our records.']);
+        }
     }
 }
--- resources/views/livewire/login-page.blade.php
+++ resources/views/livewire/login-page.blade.php
@@ -1,8 +1,8 @@
 <div class="grid place-content-center">
     <div class="border shadow-xl flex flex-col gap-3 align-middle p-10">
         <h1 class="text-center">Login</h1>
-        <input type="text" placeholder="Username" name="username">
-        <input type="password" placeholder="Password" name="password">
+        <input type="text" placeholder="Username" wire:model='username'>
+        <input type="password" placeholder="Password" wire:model='password'>
         <button class="bg-cyan-600 p-3 rounded" type="submit">Login</button>
     </div>
 </div>
