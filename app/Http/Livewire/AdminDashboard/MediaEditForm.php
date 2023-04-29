fa53fa975a641189f12fa0fa936e4764d53323a2
adishmuhammed <adishmuhammedmm@gmail.com>
2023-04-29 17:28:18 +0530

    media edit media form done

--- app/Http/Livewire/AdminDashboard/MediaEditForm.php
+++ app/Http/Livewire/AdminDashboard/MediaEditForm.php
@@ -6,8 +6,55 @@ use Livewire\Component;
 
 class MediaEditForm extends Component
 {
+    public $gallery;
+
+    public $image;
+    public $video;
+    public $type;
+    public $info;
+
+    public $imageSelected = false;
+    public $videoSelected = false;
+
+    protected $rules = [
+        'image' => 'required|image|max:526',
+        '$video' => 'required'
+    ];
+
+    public function mount()
+    {
+        $this->type = $this->gallery->type;
+        if ($this->type == 'image') {
+            $this->imageSelected = true;
+        } else {
+            $this->videoSelected =true;
+        }
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
     public function render()
     {
         return view('livewire.admin-dashboard.media-edit-form');
+    }
+
+    public function editMedia()
+    {
+        if ($this->imageSelected) {
+            $this->gallery->image = $this->image;
+        } 
+        if ($this->videoSelected) {
+            $this->gallery->video = $this->video;
+        }
+
+        $this->gallery->type = $this->type;
+        $this->gallery->info = $this->info;
+
+        $this->gallery->save();
+
+        return redirect()->route('home');
     }
 }
--- resources/views/livewire/admin-dashboard/media-edit-form.blade.php
+++ resources/views/livewire/admin-dashboard/media-edit-form.blade.php
@@ -1,3 +1,26 @@
-<div>
-    {{-- Do your work, then step back. --}}
-</div>
+<form wire:submit.prevent="updateBlog" class="max-w-md mx-auto">
+    <div class="p-6 border rounded-xl shadow-xl flex flex-col justify-center items-center gap-4">
+        <div x-show='imageSelected'>
+            <label for="image" class="block text-gray-700 font-bold mb-2">Upload Image:</label>
+            <input type="file" wire:model='image' class="py-2 px-4 border rounded-md">
+            <textarea cols="30" rows="10" wire:model='info' class="block w-full py-2 px-4 border rounded-md my-4"></textarea>
+
+            <div class="flex items-center justify-center">
+                <button type="submit"
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-colors duration-300">Upload
+                    Media</button>
+            </div>
+        </div>
+        <div x-show='videoSelected'>
+            <label for="video" class="block text-gray-700 font-bold mb-2">Upload Video:</label>
+            <input type="file" wire:model='video' class="py-2 px-4 border rounded-md">
+            <textarea cols="30" rows="10" wire:model='info' class="block w-full py-2 px-4 border rounded-md my-4"></textarea>
+
+            <div class="flex items-center justify-center">
+                <button type="submit"
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-colors duration-300">Upload
+                    Media</button>
+            </div>
+        </div>
+    </div>
+</form>
